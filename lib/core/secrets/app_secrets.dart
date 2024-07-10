54afcfab1de3f53184726f98762b2184c635f44e
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-07-10 10:25:39 +0900

    Auth feature

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+
+  static const supabaseUrl = "https://ostmshvqcobtprntoprl.supabase.co";
+  static const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdG1zaHZxY29idHBybnRvcHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1MjIzNDQsImV4cCI6MjAzNjA5ODM0NH0.nMZnVlPWo0k6QvkpnpCiRZPRTUbcpfVqzcryND9AfGk";
+}
---
+++ lib/features/auth/domain/usecases/user_sign_up_usecase.dart
@@ -0,0 +1,37 @@
+
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignUpUsecase implements UseCase<String, UserSignUpParams> {
+
+  final AuthRepository authRepository;
+  UserSignUpUsecase(this.authRepository);
+
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+        name: params.name,
+        email: params.email,
+        password: params.password,
+    );
+
+  }
+
+}
+
+class UserSignUpParams {
+
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams({
+    required this.name,
+    required this.email,
+    required this.password
+  });
+
+
+}
